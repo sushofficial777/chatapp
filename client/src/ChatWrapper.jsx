cf61275a261aa20631757b1da2c6bb1564cf42ae
Sushil <skp952580@gmail.com>
2022-11-01 11:52:59 +0530

    last commit

--- client/src/ChatWrapper.jsx
+++ client/src/ChatWrapper.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("/");
 
 const ChatWrapper = () => {
     const [username, setUsername] = useState("");
@@ -16,6 +16,8 @@ const ChatWrapper = () => {
         setShowChat(true);
       }
     };
+
+    
   
     return (
         <div>
